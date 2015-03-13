736c4a0bce59587d229b88b53c74494541e75754
Jhonatan Mark <jhoemrs@gmail.com>
2015-03-12 21:52:00 -0300

    Feito 90% de tudo

--- js/citacoes.js
+++ js/citacoes.js
@@ -25,4 +25,36 @@ $(document).ready(function ()  {
     }
 });
     
+$('#btRandom').on("click", function (e) {
+    var number = Math.floor(Math.random() * 51);
+    
+    $.ajax({
+    type: 'GET',
+    url: "/../pages/citacoes/mensagens.json",
+    dataType: 'json',
+    contentType: 'application/json',
+    crossDomain: true,
+    cache:false,
+    success: function(data) 
+    {
+        $.each(data, function(i, citacao) {
+            item = "<center><h4>" + citacao.cita + "</h4></center>"+ "<br />" + "<right><h6>"+ citacao.autor + "</h6></right>"
+
+            if (citacao.id == number) {
+                $('#boxcita').html(''),
+                $('#boxcita').append(item);
+            }
+        });
+    },
+    error:function(jqXHR, textStatus, errorThrown){
+        alert('Erro ao carregar');
+        console.log(errorThrown);
+    }
+
+});     
+    
 });
+
+});
+
+    
--- js/desafio.js
+++ js/desafio.js
@@ -1,7 +1,26 @@
 $(document).ready(function ()  {
     
-    var number = Math.floor(Math.random() * 23);
+    $.ajax({
+    type: 'GET',
+    url: "/../pages/desafio/desafiando.json",
+    dataType: 'json',
+    contentType: 'application/json',
+    crossDomain: true,
+    cache:false,
+    success: function(data) 
+    {
+        $.each(data, function(i, desafio) {
+            item = "<option value="+ desafio.id + ">" + desafio.data + "</option>";
+            $('#selectDesafio').append(item);
+            });
+    },
+    error:function(jqXHR, textStatus, errorThrown){
+        alert('Erro ao carregar');
+        console.log(errorThrown);
+    }
+});
     
+$('#selectDesafio').on('change', function() {
     $.ajax({
     type: 'GET',
     url: "/../pages/desafio/desafiando.json",
@@ -13,8 +32,9 @@ $(document).ready(function ()  {
     {
         $.each(data, function(i, desafio) {
             item = "<center><h4>" + desafio.meta + "</h4></center>";
-
-            if (desafio.id == number) {
+            
+            if (desafio.id == $('#selectDesafio').val() ) {
+                $('#blocodesafio').html(''),
                 $('#blocodesafio').append(item);
             }
         });
@@ -23,6 +43,9 @@ $(document).ready(function ()  {
         alert('Erro ao carregar');
         console.log(errorThrown);
     }
-});
+});     
     
 });
+
+});
+
